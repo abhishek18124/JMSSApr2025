08dee860d44915fecbf00081a49e1c2d09c7001d
Abhishek Srivastava <abhishek18124@iiitd.ac.in>
2025-04-14 12:52:33 +0530

    Lecture 01

--- Lecture 01/013SwitchStatement.cpp
+++ Lecture 01/013SwitchStatement.cpp
@@ -4,5 +4,25 @@ using namespace std;
 
 int main() {
 
+	char grade;
+	cin >> grade;
+
+	// if (grade == 'A') {
+	// 	cout << "good" << endl;
+	// } else if (grade == 'B') {
+	// 	cout << "average" << endl;
+	// } else if (grade == 'C') {
+	// 	cout << "bad" << endl;
+	// } else {
+	// 	cout << "invalid" << endl;
+	// }
+
+	switch (grade) {
+	case 'A': cout << "good" << endl; break;
+	case 'B': cout << "average" << endl; break;
+	case 'C': cout << "bad" << endl; break;
+	default: cout << "invalid" << endl; break;
+	}
+
 	return 0;
 }
